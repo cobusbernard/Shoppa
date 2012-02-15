cbce7d3dd3fec36518015ef4342d9caee296cc2c
Cobus Bernard <cobus.bernard@gmail.com>
2012-02-15 13:09:49 +0200

    Created basic project. Still need check the impact of excluding the gen folder on another workstation.

---
+++ src/com/cobusbernard/shoppa/ShoppaActivity.java
@@ -0,0 +1,13 @@
+package com.cobusbernard.shoppa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShoppaActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
